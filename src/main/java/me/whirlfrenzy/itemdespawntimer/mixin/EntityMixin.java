ffd6e07256406fd51ac3a57eb561e0fbc90580d3
whirlfrenzy <147884408+whirlfrenzy@users.noreply.github.com>
2023-12-24 21:48:09 +0100

    Why didn't my changes get commited correctly?

--- src/main/java/me/whirlfrenzy/itemdespawntimer/mixin/EntityMixin.java
+++ src/main/java/me/whirlfrenzy/itemdespawntimer/mixin/EntityMixin.java
@@ -1,2 +1,16 @@
-package me.whirlfrenzy.itemdespawntimer.mixin;public class EntityMixin {
+package me.whirlfrenzy.itemdespawntimer.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+    // Needed to get the onStartedTrackingBy mixin in ItemEntityMixin to work outside the dev environment
+    // + ItemEntity doesn't override the onStartedTrackingBy method
+    @Inject(method = "onStartedTrackingBy", at = @At("HEAD"))
+    public void onStartedTrackingBy(ServerPlayerEntity player, CallbackInfo ci) {}
 }
--- src/main/java/me/whirlfrenzy/itemdespawntimer/mixin/ItemEntityMixin.java
+++ src/main/java/me/whirlfrenzy/itemdespawntimer/mixin/ItemEntityMixin.java
@@ -11,9 +11,10 @@ import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemEntity.class)
-public abstract class ItemEntityMixin implements ItemEntityAccessInterface {
+public abstract class ItemEntityMixin extends EntityMixin implements ItemEntityAccessInterface {
     @Shadow public abstract int getItemAge();
 
     @Unique
@@ -29,7 +30,8 @@ public abstract class ItemEntityMixin implements ItemEntityAccessInterface {
         this.timerLabelVisibility = newTimerLabelVisibility;
     }
 
-    public void onStartedTrackingBy(ServerPlayerEntity player) {
+    @Override
+    public void onStartedTrackingBy(ServerPlayerEntity player, CallbackInfo ci) {
         PacketByteBuf buf = PacketByteBufs.create();
         buf.writeInt(((Entity)(Object)(this)).getId());
         buf.writeInt(this.getItemAge());
