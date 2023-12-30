e8b29e70888927f5878f1991e19bf067ecf23599
whirlfrenzy <147884408+whirlfrenzy@users.noreply.github.com>
2023-12-30 20:07:42 +0100

    Show infinite when the item never despawns
    Next: Move to Architectury and get a NeoForge port started

--- src/main/java/me/whirlfrenzy/itemdespawntimer/mixin/ItemEntityMixin.java
+++ src/main/java/me/whirlfrenzy/itemdespawntimer/mixin/ItemEntityMixin.java
@@ -1,5 +1,6 @@
 package me.whirlfrenzy.itemdespawntimer.mixin;
 
+import me.whirlfrenzy.itemdespawntimer.ItemDespawnTimer;
 import me.whirlfrenzy.itemdespawntimer.access.ItemEntityAccessInterface;
 import me.whirlfrenzy.itemdespawntimer.networking.PacketReceiver;
 import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
@@ -8,17 +9,37 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.network.PacketByteBuf;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin extends EntityMixin implements ItemEntityAccessInterface {
     @Shadow public abstract int getItemAge();
 
+    @Shadow private int itemAge;
     @Unique
     boolean timerLabelVisibility = true;
+
+    // -1 here is to mark that the previous tick's item age hasn't been set yet. Apparently checking if it's null is a bit weird or I'm just stupid
+    // Only used on the server in the tick method
+    @Unique
+    int previousTickItemAge = -1;
+
+    // I would like to use the itemAge field provided by the ItemEntity class, but I do want to avoid interfering with any other mod relying on that same field
+    // + Minecraft uses that field to calculate the rotation and setting that field to a new value more than 1 away from the previous value causes a noticeable "lag back"
+    // This is only used on the client by the way, the server will send packets to the client to update this field if necessary
+    @Unique
+    int modItemAge = 0;
+    @Unique
+    ArrayList<ServerPlayerEntity> playersInRange = new ArrayList<>();
 
     @Override
     public boolean item_despawn_timer$getTimerLabelVisibility() {
@@ -30,11 +51,45 @@ public abstract class ItemEntityMixin extends EntityMixin implements ItemEntityA
         this.timerLabelVisibility = newTimerLabelVisibility;
     }
 
-    @Override
-    public void onStartedTrackingBy(ServerPlayerEntity player, CallbackInfo ci) {
+    public int item_despawn_timer$getModItemAge(){
+        return this.modItemAge;
+    };
+
+    public void item_despawn_timer$setModItemAge(int modItemAge){
+        this.modItemAge = modItemAge;
+    };
+
+    @Unique
+    public void item_despawn_timer$sendItemAgePacket(ServerPlayerEntity player){
         PacketByteBuf buf = PacketByteBufs.create();
         buf.writeInt(((Entity)(Object)(this)).getId());
         buf.writeInt(this.getItemAge());
         ServerPlayNetworking.send(player, PacketReceiver.ITEM_AGE_PACKET_IDENTIIER, buf);
+    }
+
+    @Override
+    public void onStartedTrackingBy(ServerPlayerEntity player, CallbackInfo ci) {
+        item_despawn_timer$sendItemAgePacket(player);
+        playersInRange.add(player);
+    }
+
+    @Override
+    public void onStoppedTrackingBy(ServerPlayerEntity player, CallbackInfo ci){
+        playersInRange.remove(player);
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    public void tick(CallbackInfo ci) {
+        if(!this.getWorld().isClient()) {
+            if (previousTickItemAge != -1 && previousTickItemAge != (this.getItemAge() - 1) && previousTickItemAge != -32768){
+                for (ServerPlayerEntity player : playersInRange) {
+                    item_despawn_timer$sendItemAgePacket(player);
+                }
+            }
+
+            previousTickItemAge = this.getItemAge();
+        } else {
+            ++this.modItemAge;
+        }
     }
 }
