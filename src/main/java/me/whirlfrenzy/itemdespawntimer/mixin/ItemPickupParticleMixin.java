0ca4e20c368ed0c970b8495622d2b4db35ee51e7
whirlfrenzy <147884408+whirlfrenzy@users.noreply.github.com>
2024-04-16 18:07:21 +0200

    Fix #2
    Check if the entity is an ItemEntity

--- src/main/java/me/whirlfrenzy/itemdespawntimer/mixin/ItemPickupParticleMixin.java
+++ src/main/java/me/whirlfrenzy/itemdespawntimer/mixin/ItemPickupParticleMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.client.render.BufferBuilderStorage;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,6 +17,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ItemPickupParticleMixin {
     @Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/client/render/entity/EntityRenderDispatcher;Lnet/minecraft/client/render/BufferBuilderStorage;Lnet/minecraft/client/world/ClientWorld;Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/Entity;Lnet/minecraft/util/math/Vec3d;)V")
     public void disableLabelRendering(EntityRenderDispatcher dispatcher, BufferBuilderStorage bufferStorage, ClientWorld world, Entity itemEntity, Entity interactingEntity, Vec3d velocity, CallbackInfo ci){
-        ((ItemEntityAccessInterface)((ItemPickupParticleAccessor)this).getItemEntity()).item_despawn_timer$setTimerLabelVisibility(false);
+        if(itemEntity instanceof ItemEntity) {
+            ((ItemEntityAccessInterface) ((ItemPickupParticleAccessor) this).getItemEntity()).item_despawn_timer$setTimerLabelVisibility(false);
+        }
     }
 }
