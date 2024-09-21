5cc023443b7c3516605bb3869cb5301ca02b483f
whirlfrenzy <147884408+whirlfrenzy@users.noreply.github.com>
2024-09-21 11:58:05 +0200

    Item whitelist

--- common/src/main/java/whirlfrenzy/itemdespawntimer/config/ItemDespawnTimerClientConfig.java
+++ common/src/main/java/whirlfrenzy/itemdespawntimer/config/ItemDespawnTimerClientConfig.java
@@ -1,5 +1,10 @@
 package whirlfrenzy.itemdespawntimer.config;
 
+import net.minecraft.util.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ItemDespawnTimerClientConfig extends MidnightConfig {
     @Entry
     public static boolean timerVisible = true;
@@ -12,4 +17,13 @@ public class ItemDespawnTimerClientConfig extends MidnightConfig {
 
     @Entry(min = -0.5, max = 5)
     public static float nameLabelHeight = 1.25f;
+
+    @Entry
+    public static boolean useWhitelist = false;
+
+    @Entry
+    public static boolean whitelistIsBlacklist = false;
+
+    @Entry
+    public static List<Identifier> whitelistedItems = new ArrayList<>();
 }
