d236774dfd3d74bdb99aae294dfd0ba36f48ab9f
whirlfrenzy <147884408+whirlfrenzy@users.noreply.github.com>
2024-10-14 13:39:35 +0200

    Decrease ItemEntityRendererMixin priority and inject at head
    To make it compatible with Item Physic Full and Lite (#7)

--- common/src/main/java/whirlfrenzy/itemdespawntimer/mixin/ItemEntityRendererMixin.java
+++ common/src/main/java/whirlfrenzy/itemdespawntimer/mixin/ItemEntityRendererMixin.java
@@ -10,9 +10,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import whirlfrenzy.itemdespawntimer.rendering.LabelRenderer;
 
-@Mixin(ItemEntityRenderer.class)
+// Priority is set to 999 (one below the default) to still render the labels in case any mods use ci.cancel() (such as Item Physic Full and Lite)
+// Higher priorities get added first and thus my mixin should be added after Item Physic's mixin and end up above it
+@Mixin(value = ItemEntityRenderer.class, priority = 300)
 public abstract class ItemEntityRendererMixin {
-    @Inject(at = @At(value = "TAIL"), method = "render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
+    @Inject(at = @At(value = "HEAD"), method = "render(Lnet/minecraft/entity/ItemEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
     public void renderTextLabels(ItemEntity itemEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int lightLevel, CallbackInfo ci) {
         LabelRenderer.renderTextLabels(itemEntity, matrixStack, vertexConsumerProvider, lightLevel);
     }
